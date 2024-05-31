51299b8c20d95682a1ed92832f36e2aae2b22cd6
Michal <michal.front@outlook.com>
2024-05-31 21:26:13 +0200

    Add wrapper styling and base colors

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,15 +1,24 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+:root {
+  --primary-color: #c8ce9f;
+  --secondary-color: #691d04;
+  --success-color: #28a745;
+  --danger-color: #dc3545;
+  --warning-color: #ffc107;
+  --info-color: #17a2b8;
+  --light-color: #db4206;
+  --dark-color: #82350b;
+}  
+
+body {
     margin: 0;
     padding: 2rem 0;
     background: teal;
     font-family: Open-Sans, Helvetica, Sans-Serif;
     box-sizing: border-box;
-    display: flex;
-    justify-content: center;
   }
 `;
- 
+
 export default GlobalStyle;
